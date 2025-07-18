56abe4211ce23f4f02af9fc26ee8f23411b244c0
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-07-18 13:05:24 +0800

    initial commit for petslikev2 enhanced performance + new integrations

---
+++ backend/src/app.ts
@@ -0,0 +1,28 @@
+import Fastify from 'fastify';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const fastify = Fastify({
+    logger: true
+});
+
+try{
+    fastify.listen({
+        port: process.env.http_port? Number(process.env.http_port) : 3000,
+        host: process.env.host? String(process.env.host) : `127.0.0.1`
+    }).then(()=> {
+        fastify.log.info(`Server is running on port: ${process.env.http_port}, host: ${process.env.host}`)
+    });
+
+}catch(err: unknown){
+    if(err instanceof Error){
+        fastify.log.error(`Error occured while attempting to run the server, error: ${err}`)
+    }else{
+        fastify.log.error(`Unknown error occured, error: ${err}`)
+    }
+    fastify.close().then(()=> {
+        process.exit(1);
+    });
+}
+
