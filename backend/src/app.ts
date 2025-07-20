4bed8a5e6a6094d8807d67eacdd73925f6d705b8
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-07-20 15:18:39 +0800

    removed rebunant .then

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,10 +20,8 @@ try{
     fastify.listen({
         port: process.env.http_port? Number(process.env.http_port) : 3000,
         host: process.env.host? String(process.env.host) : `127.0.0.1`
-    }).then(()=> {
-        fastify.log.info(`Server is running on port: ${process.env.http_port}, host: ${process.env.host}`)
     });
-
+    fastify.log.info(`Server is running on port: ${process.env.http_port}, host: ${process.env.host}`);
 }catch(err: unknown){
     if(err instanceof Error){
         fastify.log.error(`Error occured while attempting to run the server, error: ${err}`)
