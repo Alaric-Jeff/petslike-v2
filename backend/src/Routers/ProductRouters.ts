146097e657a72efd733304b4a39ff7662e251e1d
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-07-25 15:24:51 +0800

    route + new package

--- backend/src/Routers/ProductRouters.ts
+++ backend/src/Routers/ProductRouters.ts
@@ -1,17 +1,28 @@
 import { FastifyInstance } from "fastify";
 import {ProductSchema} from "../Schemas/ProductSchemas/AddProductSchema.js";
+import { DeleteProductSchema } from "../Schemas/ProductSchemas/DeleteProductSchema.js";
+import DeleteProductController from "../Controllers/ProductControllers/DeleteProductController.js";
 import AddProductController from "../Controllers/ProductControllers/AddProductController.js";
 
 async function ProductRouters(fastify: FastifyInstance) {
   fastify.route({
     method: 'POST',
-    url: 'api/products/add-products',
+    url: 'api/products/add-product',
     schema: {
       body: ProductSchema
     },
     preHandler: fastify.compress,
     handler: AddProductController
   });
+
+  fastify.route({
+    method: 'POST',
+    url: 'api/products/delete-product',
+    schema: {
+      body: DeleteProductSchema
+    }, handler: DeleteProductController
+  });
+
 }
 
 export default ProductRouters;
