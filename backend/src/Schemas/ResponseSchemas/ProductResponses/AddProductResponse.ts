b32130fea3183f5edbc352eaddaa0de2e3615582
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-07-27 12:47:01 +0800

    schemas

--- backend/src/Schemas/ResponseSchemas/ProductResponses/AddProductResponse.ts
+++ backend/src/Schemas/ResponseSchemas/ProductResponses/AddProductResponse.ts
@@ -5,7 +5,7 @@ import { ProductSchema } from "../../ProductSchemas/AddProductSchema.js";
 const AddProductResponse = Type.Object({
     message: Type.String(),
     success: Type.Boolean(),
-    product: ProductSchema       
+    product: ProductSchema
 });
 
 export default AddProductResponse;
